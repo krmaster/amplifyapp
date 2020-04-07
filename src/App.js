26ebf0b7087c71602db1ba270139ba84ce272591
Christian Sandborg <christian.sandborg@skill-it.se>
2020-04-07 15:59:07 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YES! Edit <code>src/App.js</code> and save to reload.
+          YES! Edit <code>src/App.js</code> and save to reload. 2nd Commit!
         </p>
         <a
           className="App-link"
